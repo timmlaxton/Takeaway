f12b5bcb000157e95a9d436d6a6323deb856b8da
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-02-26 13:15:09 +0000

    added added layout component

--- takeaway-app/src/App.js
+++ takeaway-app/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+import Layout from './components/Layout/Layout'
+
+class App extends Component {
+  render () {
   return (
-    <div className="App">
-      
+    <div >
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
-
+}
 export default App;
