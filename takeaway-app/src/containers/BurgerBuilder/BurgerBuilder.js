6972c2cfcb794c796218fb842107bcada1ab7d39
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-03-03 14:03:13 +0000

    disabled rmove button

--- takeaway-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ takeaway-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,6 +37,9 @@ class BurgerBuilder extends Component {
 
    removeIngredientHandler = (type) => {
     const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) {
+        return;
+    }
     const updatedCount = oldCount - 1;
     const updatedIngredients = {
         ...this.state.ingredients
@@ -53,12 +56,19 @@ class BurgerBuilder extends Component {
    
    
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
                 ingredientAdded={this.addIngredientHandler}
-                ingredientRemoved={this.removeIngredientHandler}/>
+                ingredientRemoved={this.removeIngredientHandler}
+                disabled={disabledInfo}/>
 
             </Aux>
         );
